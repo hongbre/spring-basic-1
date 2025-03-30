1a3a35d8de5e5d5a239a2c46fd886687256f60c8
hongbre <hongbre@gmail.com>
2025-03-30 12:48:13 +0900

    회원 도메인 테스트

--- src/main/java/spring/basic/BasicApplication.java
+++ src/main/java/spring/basic/BasicApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DBasicApplication {
+public class BasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicApplication.class, args);
