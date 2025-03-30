068517caac336a9a829465b93a8f43595d14dc99
hongbre <hongbre@gmail.com>
2025-03-30 12:06:38 +0900

    Project 생성

---
+++ src/main/java/spring/basic/BasicApplication.java
@@ -0,0 +1,13 @@
+package spring.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplication.class, args);
+	}
+
+}
