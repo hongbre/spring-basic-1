a1aedc8bc1be78c93e9acf64104dd41cafeb2873
hongbre <hongbre@gmail.com>
2025-04-13 15:15:45 +0900

    spring으로 변경

--- src/main/java/spring/basic/AppConfig.java
+++ src/main/java/spring/basic/AppConfig.java
@@ -1,5 +1,7 @@
 package spring.basic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.basic.discount.DiscountPolicy;
 import spring.basic.discount.FixDiscountPolicy;
 import spring.basic.discount.RateDiscountPolicy;
@@ -10,20 +12,25 @@ import spring.basic.member.MemoryMemberRepository;
 import spring.basic.order.OrderService;
 import spring.basic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
