3fbc06264cc9b72e69a80618d23796eea86920c2
hongbre <hongbre@gmail.com>
2025-04-26 22:47:52 +0900

    @Configuration은 싱글톤을 보장하는가

--- src/main/java/spring/basic/AppConfig.java
+++ src/main/java/spring/basic/AppConfig.java
@@ -15,16 +15,19 @@ import spring.basic.order.OrderServiceImpl;
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
