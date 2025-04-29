f2a861b47f51c4f971143f8451e2d8b39d8b03e0
hongbre <hongbre@gmail.com>
2025-04-29 21:23:01 +0900

    @ComponentScan의 basePackages 적용

--- src/main/java/spring/basic/AutoAppConfig.java
+++ src/main/java/spring/basic/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "spring.basic",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) //@Configuration 도 @Component 이기 때문에 Scan 대상에서 제외
 public class AutoAppConfig {
