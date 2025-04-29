111eb58396e3dc65009530f115dc7fcc6385acc8
hongbre <hongbre@gmail.com>
2025-04-29 22:00:46 +0900

    수동 등록 Bean과 자동 등록 Bean 충돌 발생

--- src/main/java/spring/basic/AutoAppConfig.java
+++ src/main/java/spring/basic/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package spring.basic;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import spring.basic.member.MemberRepository;
+import spring.basic.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -10,5 +13,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) //@Configuration 도 @Component 이기 때문에 Scan 대상에서 제외
 public class AutoAppConfig {
-
+/*    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }*/
 }
