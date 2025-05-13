1b2caf923391f2a5fc7f16f7be9286862724aad7
hongbre <hongbre@gmail.com>
2025-05-13 15:27:49 +0900

    Bean Life Cycle Method -> Annotation

--- src/test/java/spring/basic/lifecycle/NetworkClient.java
+++ src/test/java/spring/basic/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package spring.basic.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -20,12 +23,15 @@ public class NetworkClient {
     public void disConnect() {
         System.out.println("close + " + url);
     }
+
+    @PostConstruct
     public void init()  {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close()  {
         System.out.println("NetworkClient.close");
         disConnect();
