9e2d817656760e930b1d7459e238280abd54e0be
hongbre <hongbre@gmail.com>
2025-05-30 20:50:40 +0900

    웹 스코프의 request 스코프와 Provider

--- src/main/java/spring/basic/AppConfig.java
+++ src/main/java/spring/basic/AppConfig.java
@@ -13,26 +13,26 @@ import spring.basic.order.OrderServiceImpl;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public MemberService memberService() {
-        System.out.println("call AppConfig.memberService");
-        return new MemberServiceImpl(memberRepository());
-    }
-
-    @Bean
-    public OrderService orderService() {
-        System.out.println("call AppConfig.orderService");
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        System.out.println("call AppConfig.memberRepository");
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
-    public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        System.out.println("call AppConfig.memberService");
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//
+//    @Bean
+//    public OrderService orderService() {
+//        System.out.println("call AppConfig.orderService");
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        System.out.println("call AppConfig.memberRepository");
+//        return new MemoryMemberRepository();
+//    }
+//
+//    @Bean
+//    public DiscountPolicy discountPolicy() {
+//        return new RateDiscountPolicy();
+//    }
 }
---
+++ src/main/java/spring/basic/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package spring.basic.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import spring.basic.common.MyLogger;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
